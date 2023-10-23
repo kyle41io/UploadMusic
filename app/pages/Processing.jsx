7adcaee4f4fa3f4cc4ba28b563bd7bee34f27582
kyle41io <vankien41hust@gmail.com>
2023-10-23 16:26:49 +0700

    fix: text overflow

--- app/pages/Processing.jsx
+++ app/pages/Processing.jsx
@@ -61,16 +61,9 @@ const Processing = () => {
           <h2 className="text-[#0F0F0F] font-semibold text-base">
             Congratulation, you’ve uploaded successfully !
           </h2>
-          <div
-            className="max-w-full flex text-sm gap-1 "
-            style={{ wordWrap: "break-word" }}
-          >
-            <p className="overflow-hidden whitespace-nowrap overflow-ellipsis">
-              {title}
-            </p>{" "}
-            -{" "}
-            <p className="overflow-hidden whitespace-nowrap overflow-ellipsis">
-              {artist ? artist : "N/A"}
+          <div className="max-w-full flex text-sm gap-1 ">
+            <p className="break-all">
+              {title} - {artist ? artist : "N/A"}
             </p>
           </div>
           <div className="flex gap-4 text-xs text-[#979797]">
